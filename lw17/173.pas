7103c26ba735b7ffe5f7749915f170ed25b38053
ravenorth <61602512+ravenorth@users.noreply.github.com>
2020-04-18 04:07:18 +0300

    Update 173.pas

--- lw17/173.pas
+++ lw17/173.pas
@@ -1,72 +1,76 @@
 PROGRAM Stat(INPUT, OUTPUT);
+CONST
+  MaxInteger = 32767;
 VAR
-  N, D, MaxInteger, Min, Max, Ave, Sum, Count: INTEGER;
+  Number, Digit, Min, Max, Average, Sum, Count: INTEGER;
   OverFlow: BOOLEAN;
-PROCEDURE ReadDigit(VAR F: TEXT; VAR D: INTEGER);
+
+PROCEDURE ReadDigit(VAR FIn: TEXT; VAR Digit: INTEGER);
 VAR
   Ch: CHAR;
 BEGIN {ReadDigit}
-  D := -1;
-  IF NOT EOLN(F)
+  Digit := -1;
+  IF NOT EOLN
   THEN
     READ(Ch);
-  IF Ch = '0' THEN D := 0 ELSE
-  IF Ch = '1' THEN D := 1 ELSE
-  IF Ch = '2' THEN D := 2 ELSE
-  IF Ch = '3' THEN D := 3 ELSE
-  IF Ch = '4' THEN D := 4 ELSE
-  IF Ch = '5' THEN D := 5 ELSE
-  IF Ch = '6' THEN D := 6 ELSE
-  IF Ch = '7' THEN D := 7 ELSE
-  IF Ch = '8' THEN D := 8 ELSE
-  IF Ch = '9' THEN D := 9
+  IF Ch = '0' THEN Digit := 0 ELSE
+  IF Ch = '1' THEN Digit := 1 ELSE
+  IF Ch = '2' THEN Digit := 2 ELSE
+  IF Ch = '3' THEN Digit := 3 ELSE
+  IF Ch = '4' THEN Digit := 4 ELSE
+  IF Ch = '5' THEN Digit := 5 ELSE
+  IF Ch = '6' THEN Digit := 6 ELSE
+  IF Ch = '7' THEN Digit := 7 ELSE
+  IF Ch = '8' THEN Digit := 8 ELSE
+  IF Ch = '9' THEN Digit := 9
 END; {ReadDigit}
-PROCEDURE ReadNumber(VAR F: TEXT; VAR N: INTEGER); 
+
+PROCEDURE ReadNumber(VAR FIn: TEXT; VAR Number: INTEGER); 
 BEGIN{ReadNumber}
-  N := 0;
-  ReadDigit(INPUT, D);
-  WHILE ((D <> -1) AND (N <> -1))
+  Number := 0;
+  ReadDigit(INPUT, Digit);
+  WHILE (Digit <> -1) AND (Number <> -1) 
   DO
     BEGIN
-      IF (D  <= (MaxInteger - N * 10)) 
+      IF (Digit  <= (MaxInteger - Number * 10)) 
       THEN
-        N := N * 10 + D 
+        Number := Number * 10 + Digit 
       ELSE
-        N := -1;  
-      ReadDigit(INPUT, D)
+        Number := -1;  
+      ReadDigit(INPUT, Digit)
     END
 END;{ReadNumber}
+
 BEGIN {Stat}
-  MaxInteger := 32767;
   Min := 32767;
   Max := 0;
   OverFlow := FALSE;
-  WHILE ((NOT EOLN) AND (OverFlow = FALSE))
+  WHILE (NOT EOLN) AND (NOT OverFlow)
   DO
     BEGIN
-      ReadNumber(INPUT, N);
-      IF (N  <= (MaxInteger - Sum)) AND (N <> -1)
+      ReadNumber(INPUT, Number);
+      IF (Number  <= (MaxInteger - Sum)) AND (Number <> -1)
       THEN
         BEGIN
-          Sum := Sum + N;
+          Sum := Sum + Number;
           Count := Count + 1;
-          IF N < Min
+          IF Number < Min
           THEN
-            Min := N;
-          IF N > Max
+            Min := Number;
+          IF Number > Max
           THEN
-            Max := N
+            Max := Number
         END
       ELSE
         OverFlow := TRUE
     END;
-  IF (OverFlow = FALSE) 
+  IF (NOT OverFlow) 
   THEN
     BEGIN
-      Ave := (Sum * 100) DIV Count;
+      Average := (Sum * 100) DIV Count;
       WRITELN('Min: ', Min);
       WRITELN('Max: ', Max);
-      WRITELN('Average: ', Ave DIV 100, '.', Ave MOD 100)
+      WRITELN('Average: ', Average DIV 100, '.', Average MOD 100)
     END
   ELSE
     WRITELN('OVERFLOW ERROR')
