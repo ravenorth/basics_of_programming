8c3a8c2637a16f4dc14570f02b3eaa04d9744486
ravenorth <61602512+ravenorth@users.noreply.github.com>
2020-06-03 03:08:37 +0300

    Update 231.pas

--- lw23/231.pas
+++ lw23/231.pas
@@ -15,10 +15,8 @@ BEGIN {InsertSort2}
     BEGIN
       NEW(NewPtr);
       READ(NewPtr^.Key);
-      {2.1. Поместить NewPtr в надлежащее место}
       Prev := NIL;
       Curr := FirstPtr;
-      {2.1.1 Найдем значение Prev и Curr, такие что Prev^.Key <= NewPtr^.Key <= Curr^.Key}
       Found := FALSE;
       WHILE (Curr <> NIL) AND (NOT Found)
       DO
@@ -37,7 +35,6 @@ BEGIN {InsertSort2}
       ELSE
         Prev^.Next := NewPtr
     END;
-  {2.2. Печать значений начиная с FirstPtr^.Key}
   NewPtr := FirstPtr;
   WHILE NewPtr <> NIL
   DO
